004d663c9b3fc1dacd9a56dcf013d2e0a434e7dd
Douglas Oliveira (Ultrabook) <dougmoroli@gmail.com>
2022-05-30 16:14:59 -0300

    criando conexao com o banco de dados

---
+++ pos-java-jdbc/src/main/java/conexaoJDBC/SingleConnection.java
@@ -0,0 +1,42 @@
+package conexaoJDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnection {
+
+	private static String url = "jdbc:postgresql://localhost:5433/posjava";
+	private static String password = "admin";
+	private static String user = "postgres";
+	private static Connection connection = null;
+
+	static {
+		conectar(); // qualquer lugar que eu invocar o SingleConnection, ele vai chamar o conectar
+					// ()
+	}
+
+	public SingleConnection() {
+		conectar();
+	}
+
+	private static void conectar() {
+		try {
+			if (connection == null) {
+				// iniciar carregamento do driver de conexão que iremos usar
+				Class.forName("org.postgresql.Driver");
+				connection = DriverManager.getConnection(url, user, password);
+				connection.setAutoCommit(false); //falso para que decidir quando salvar as operações do banco
+				System.out.println("Conexao estabelecida com sucesso");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static Connection getConnection() {
+		return connection;
+	}
+
+}
