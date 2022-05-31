bd571ab091cdfc5733121de0dd2fc21e344962dc
Douglas Oliveira (Ultrabook) <dougmoroli@gmail.com>
2022-05-31 15:48:18 -0300

    Criando INSERT no banco de dados

--- pos-java-jdbc/src/main/java/dao/UserPosDAO.java
+++ pos-java-jdbc/src/main/java/dao/UserPosDAO.java
@@ -1,14 +1,40 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import conexaoJDBC.SingleConnection;
+import model.Userposjava;
 
 public class UserPosDAO {
 
 	private Connection connection;
-	
+
 	public UserPosDAO() {
 		connection = SingleConnection.getConnection();
+	}
+
+	public void salvar(Userposjava userposjava) {
+		String sql = "INSERT INTO userposjava (id, nome, email) values (?,?,?)"; // valores com interrogação para
+																					// resgatar do obejto
+		try {
+			PreparedStatement insert = connection.prepareStatement(sql);
+			insert.setLong(1, userposjava.getId());
+			insert.setString(2, userposjava.getNome());
+			insert.setString(3, userposjava.getEmail());
+			insert.execute();
+
+			connection.commit(); // salva no banco
+
+		} catch (SQLException e) {
+			try {
+				connection.rollback();// reverte a operação caso dê erro
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+
 	}
 }
--- pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJDBC.java
+++ pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJDBC.java
@@ -2,13 +2,22 @@ package pos_java_jdbc.pos_java_jdbc;
 
 import org.junit.Test;
 
-import conexaoJDBC.SingleConnection;
+import dao.UserPosDAO;
+import model.Userposjava;
 
 public class TesteBancoJDBC {
 
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+		
+		userposjava.setId(6L);
+		userposjava.setNome("Mateus");
+		userposjava.setEmail("mateus@gmail.com");
+		
+		
+		userPosDAO.salvar(userposjava);
 	}
 
 }
